f7dca9226dd924cf878f9448ab072a0db698c379
celine-felicit <celine.giese@protonmail.com>
2025-04-10 00:06:03 +0100

    Finishing up dissertation draft
    Adding conclusion and DAG, rewriting some sections and checking visualisations

---
+++ References_Dissertation.R
@@ -0,0 +1,9 @@
+#REFERENCES DISSERTATION#
+#Datasets
+Eck, Kristine and Therése Pettersson (2018) Organized Violence, 1989-2017 and the Data Generation Process. Journal of Peace Research 55(4)
+
+Dyadic Dimensions of Armed Conflict, 1946-2007
+Author(s): Lotta Harbom, Erik Melander and Peter Wallensteen
+Source:Journal of Peace Research , Sep., 2008, Vol. 45, No. 5 (Sep., 2008), pp. 697-710
+Published by: Sage Publications, Ltd.
+
